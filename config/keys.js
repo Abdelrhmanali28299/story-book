26a3d6003a2fb1418f91a60e93f9b894c537f922
abdoali28299 <abdoali28299@gmail.com>
2018-08-14 13:39:03 +0200

    start

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://abdoali28299:Abdo282990ali@ds119422.mlab.com:19422/stroybooks-dev',
+    googleClientID: '741116258662-m0e94v7ok74gbkqr171iod868gleiep0.apps.googleusercontent.com',
+    googleClientSecret: '5J5QV5yl2EuMieBlu6lL8Pbo'
+}
