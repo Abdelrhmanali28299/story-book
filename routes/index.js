b8784ac8f6b32538e3ede03c6b3c5d2939de7d95
abdoali28299 <abdoali28299@gmail.com>
2018-09-02 00:22:00 +0200

    start

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router =  express.Router()
 
 router.get('/', (req, res) => {
     res.send('It Works!')
-  })
+})
+
+router.get('/dashboard', (req, res) => {
+    res.send('dashboard')
+})
 
 module.exports = router
