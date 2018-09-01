e5fdd8bdb623340642dd7b6ff9d2e966e3fcf907
abdoali28299 <abdoali28299@gmail.com>
2018-09-01 17:52:05 +0200

    start

--- app.js
+++ app.js
@@ -43,5 +43,5 @@ app.use('/auth', auth)
 const port = process.env.PORT || 5050
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
+  console.log(`Server  started on port ${port}`)
 })
