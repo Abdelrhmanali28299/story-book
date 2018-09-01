111e3f8379bab46e2c18b6d94e610169bc9a6c63
abdoali28299 <abdoali28299@gmail.com>
2018-09-02 01:33:09 +0200

    fronet end

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router =  express.Router()
 
 router.get('/', (req, res) => {
-    res.rendre('welcome')
+    res.render('welcome')
 })
 
 router.get('/dashboard', (req, res) => {
