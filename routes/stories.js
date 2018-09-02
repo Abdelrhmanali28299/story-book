44fa3c6d2eb5d08414f7e25fd52c9e2d29b469f3
abdoali28299 <abdoali28299@gmail.com>
2018-09-02 17:18:39 +0200

    fronet end

--- routes/stories.js
+++ routes/stories.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const passport = require('passport')
-const router =  express.Router()
+const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('stories/index')
+})
+
+router.get('/add', (req, res) => {
+    res.render('stories/add.ejs')
 })
 
 module.exports = router
