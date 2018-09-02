9688eaccf9ab6cd09ec4b11f5a280c055ec04569
abdoali28299 <abdoali28299@gmail.com>
2018-09-02 23:12:40 +0200

    back end

--- routes/stories.js
+++ routes/stories.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Story = require('Story')
-const User = require('User')
+const User = require('../models/User')
+const Story = require('../models/Story')
 const { ensureAuthenticated } = require('../helpers/auth')
 
 const router = express.Router()
