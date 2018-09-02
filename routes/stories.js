625c425eeb9b0f8fff3399a2dc1c96864541ec1c
abdoali28299 <abdoali28299@gmail.com>
2018-09-02 21:14:57 +0200

    fronet end

--- routes/stories.js
+++ routes/stories.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const {ensureAuthenticated} = require('../helpers/auth')
 const Story = require('../models/Story')
-const User = require('../models/User')
+const User = require('../models/User') 
 
 const router = express.Router()
 
