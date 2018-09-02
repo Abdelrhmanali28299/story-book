5c2fc799b9e88efde7ccc84c330510a3a2df11d6
abdoali28299 <abdoali28299@gmail.com>
2018-09-02 23:32:25 +0200

    back end

--- models/story.js
+++ models/story.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const storySchema = new Schema({
-    titel: {
+    title: {
         type: String,
         required: true
     },
