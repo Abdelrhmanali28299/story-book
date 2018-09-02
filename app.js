260c7abd133db7f1be292d084a3c65527bdd1d8c
abdoali28299 <abdoali28299@gmail.com>
2018-09-02 14:22:56 +0200

    fronet end

--- app.js
+++ app.js
@@ -34,10 +34,12 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+/*
 app.use((req, res, next) => {
   res.locals.user = req.user || null
   next()
 })
+*/
 
 app.use('/', index)
 app.use('/auth', auth)
